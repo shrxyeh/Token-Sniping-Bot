ed34ddd39f02992263a4c19bb640f81ddb44e194
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-12-18 14:23:32 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,8 @@ const Token = artifacts.require("Token")
 
 module.exports = async function (deployer) {
 
-    const name = "DApp University"
-    const symbol = "DAPPU"
+    const name = "Shreyash"
+    const symbol = "shrxyeh"
     const supply = web3.utils.toWei('1000', 'ether') // 1000 Tokens
 
     await deployer.deploy(Token, name, symbol, supply)
